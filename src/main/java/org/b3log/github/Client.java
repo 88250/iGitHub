ccb96139ea01398f5e0df93b8865d0f6d8d4c161
Liang Ding <d@b3log.org>
2020-01-04 00:11:28 +0800

    :bookmark: 发布 Solo v3.6.8

--- src/main/java/org/b3log/github/Client.java
+++ src/main/java/org/b3log/github/Client.java
@@ -1,51 +1,31 @@
 package org.b3log.github;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 /**
  * GitHub client.
  *
  * @author <a href="https://hacpai.com/member/88250">Liang Ding</a>
- * @version 2.0.0.2, Jan 30, 2019
+ * @version 3.0.0.0, Jan 4, 2020
  */
 public class Client {
 
-    private static final Map<String, Integer> issueNameSort = new HashMap<>();
-
-    static {
-        issueNameSort.put("crazy", 0);
-        issueNameSort.put("feature", 1);
-        issueNameSort.put("skin", 2);
-        issueNameSort.put("theme", 3);
-        issueNameSort.put("enhancement", 4);
-        issueNameSort.put("doc", 5);
-        issueNameSort.put("development", 6);
-        issueNameSort.put("bug", 7);
-        issueNameSort.put("default", 8);
-    }
-
     private static final String MILESTONE_NUM;
-
     private static final String REPOS;
-
     private static final String USER_NAME;
-
     private static final String ISSUE_STATE;
 
     static {
@@ -72,6 +52,8 @@ public class Client {
     }
 
     public static void main(final String[] args) throws Exception {
+        loadLables();
+
         String authStr = USER_NAME + ':' + args[0];
         authStr = Base64.getEncoder().encodeToString(authStr.getBytes());
 
@@ -86,164 +68,133 @@ public class Client {
         final StringBuilder skinBuilder = new StringBuilder();
         final StringBuilder themeBuilder = new StringBuilder();
 
-        try {
-            System.out.println("Retriving issues....");
-            System.out.println();
-            int page = 1;
-            int count = 0;
-            while (true) {
-                final HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + REPOS + "/issues?"
-                        + "milestone=" + MILESTONE_NUM + "&state=" + ISSUE_STATE + "&direction=asc&page=" + page);
-                page++;
+        System.out.println("Retrieving issues....");
+        System.out.println();
+        int page = 1;
+        int count = 0;
+        while (true) {
+            final HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + REPOS + "/issues?"
+                    + "milestone=" + MILESTONE_NUM + "&state=" + ISSUE_STATE + "&direction=asc&page=" + page);
+            page++;
+            httpGet.addHeader("Authorization", "Basic " + authStr);
+            final HttpResponse response = httpClient.execute(httpGet);
+            HttpEntity entity = response.getEntity();
+            String content = IOUtils.toString(entity.getContent(), StandardCharsets.UTF_8);
 
-                httpGet.addHeader("Authorization", "Basic " + authStr);
+            final JSONArray json = new JSONArray(content);
+            if (json.length() < 1) {
+                break;
+            }
 
-                final HttpResponse response = httpClient.execute(httpGet);
-
-                HttpEntity entity = response.getEntity();
-                String content = IOUtils.toString(entity.getContent(), "UTF-8");
-
-                final JSONArray json = new JSONArray(content);
-                if (json.length() < 1) {
-                    break;
-                }
-
-                for (int i = 0; i < json.length(); i++) {
-                    final JSONObject issue = json.getJSONObject(i);
-                    final JSONArray labels = issue.getJSONArray("labels");
-
-                    final StringBuilder liBuilder = new StringBuilder();
-                    String startIssueName = "default";
-
-                    final StringBuilder labelBuilder = new StringBuilder();
-                    for (int j = 0; j < labels.length(); j++) {
-                        labelBuilder.append("<span style='");
-                        final JSONObject label = labels.getJSONObject(j);
-                        final String color = label.getString("color");
-                        final String name = label.getString("name");
-
-                        startIssueName = getStartLabelName(name, startIssueName);
-
-                        labelBuilder.append("background: #").append(color).append(" !important;color:#FFFFFF !important;padding: 1px 4px;'>").
-                                append(name).append("</span>");
-
-                        if (j < labels.length() - 1) {
-                            labelBuilder.append("&nbsp;");
-                        }
-                    }
-
-                    liBuilder.append("    <li><a href=\"").append(issue.getString("html_url")).append("\">").
-                            append(issue.optString("number")).append("</a> ").
-                            append(issue.getString("title")).append(" ").append(labelBuilder.toString()).append("</li>").append("\n");
-
-                    switch (startIssueName) {
-                        case "feature":
-                            featureBuilder.append(liBuilder.toString());
-                            count++;
-                            break;
-                        case "bug":
-                            bugBuilder.append(liBuilder.toString());
-                            count++;
-                            break;
-                        case "skin":
-                            skinBuilder.append(liBuilder.toString());
-                            count++;
-                            break;
-                        case "theme":
-                            themeBuilder.append(liBuilder.toString());
-                            count++;
-                            break;
-                        case "enhancement":
-                            enhancementBuilder.append(liBuilder.toString());
-                            count++;
-                            break;
-                        case "development":
-                            developmentBuilder.append(liBuilder.toString());
-                            count++;
-                            break;
-                        case "doc":
-                            docBuilder.append(liBuilder.toString());
-                            count++;
-                            break;
-                        default:
-                            System.err.println("The label [" + startIssueName + ", issue=" + issue.getString("number")
-                                    + "] invalid");
-                    }
+            for (int i = 0; i < json.length(); i++) {
+                final JSONObject issue = json.getJSONObject(i);
+                final String label = issue.getJSONArray("labels").optJSONObject(0).optString("name");
+                final StringBuilder liBuilder = new StringBuilder().append("* [").append(issue.getString("title")).append("](").append(issue.getString("html_url")).append(")\n");
+                switch (label) {
+                    case "引入特性":
+                        featureBuilder.append(liBuilder.toString());
+                        count++;
+                        break;
+                    case "修复缺陷":
+                        bugBuilder.append(liBuilder.toString());
+                        count++;
+                        break;
+                    case "改进皮肤":
+                        skinBuilder.append(liBuilder.toString());
+                        count++;
+                        break;
+                    case "改进主题":
+                        themeBuilder.append(liBuilder.toString());
+                        count++;
+                        break;
+                    case "改进功能":
+                        enhancementBuilder.append(liBuilder.toString());
+                        count++;
+                        break;
+                    case "开发重构":
+                        developmentBuilder.append(liBuilder.toString());
+                        count++;
+                        break;
+                    case "文档相关":
+                        docBuilder.append(liBuilder.toString());
+                        count++;
+                        break;
+                    default:
+                        System.err.println("The label [" + label + ", issue=" + issue.optString("number") + "] is invalid");
+                        System.exit(-1);
                 }
             }
-
-            System.out.println("<ul>");
-            if (featureBuilder.length() > 0) {
-                featureBuilder.deleteCharAt(featureBuilder.length() - 1);
-                System.out.println(featureBuilder.toString());
-            }
-
-            if (skinBuilder.length() > 0) {
-                skinBuilder.deleteCharAt(skinBuilder.length() - 1);
-                System.out.println(skinBuilder.toString());
-            }
-
-            if (themeBuilder.length() > 0) {
-                themeBuilder.deleteCharAt(themeBuilder.length() - 1);
-                System.out.println(themeBuilder.toString());
-            }
-
-            if (enhancementBuilder.length() > 0) {
-                enhancementBuilder.deleteCharAt(enhancementBuilder.length() - 1);
-                System.out.println(enhancementBuilder.toString());
-            }
-
-            if (docBuilder.length() > 0) {
-                docBuilder.deleteCharAt(docBuilder.length() - 1);
-                System.out.println(docBuilder.toString());
-            }
-
-            if (developmentBuilder.length() > 0) {
-                developmentBuilder.deleteCharAt(developmentBuilder.length() - 1);
-                System.out.println(developmentBuilder.toString());
-            }
-
-            if (bugBuilder.length() > 0) {
-                bugBuilder.deleteCharAt(bugBuilder.length() - 1);
-                System.out.println(bugBuilder.toString());
-            }
-
-            System.out.println("</ul>");
-            System.out.println(count + " issues totally.");
-
-            // printResponseHeader(response);
-            // printResponseContent(entity);
-        } finally {
-            // When HttpClient instance is no longer needed,
-            // shut down the connection manager to ensure
-            // immediate deallocation of all system resources
-            httpClient.getConnectionManager().shutdown();
-        }
-    }
-
-    private static void printResponseContent(HttpEntity entity) throws JSONException, IOException, IllegalStateException {
-        String content = IOUtils.toString(entity.getContent());
-        final JSONArray json = new JSONArray(content);
-        System.out.println(json.toString(4));
-
-        EntityUtils.consume(entity);
-    }
-
-    private static void printResponseHeader(final HttpResponse response) {
-        System.out.println(response.getStatusLine());
-        final Header[] responseHeaders = response.getAllHeaders();
-        for (int i = 0; i < responseHeaders.length; i++) {
-            final Header header = responseHeaders[i];
-            System.out.println(header.getName() + ": " + header.getValue());
-        }
-    }
-
-    private static String getStartLabelName(final String labelName, final String existLabelName) {
-        Integer labelNum = issueNameSort.get(labelName);
-        if (null == labelNum) {
-            labelNum = 0;
         }
 
-        return labelNum > issueNameSort.get(existLabelName) ? existLabelName : labelName;
+        System.out.println("## v" + getVersion() + " / " + new SimpleDateFormat("yyyy-MM-dd").format(new Date()) + "\n");
+
+        if (featureBuilder.length() > 0) {
+            System.out.println("### 引入特性\n");
+            System.out.println(featureBuilder.toString());
+        }
+
+        if (skinBuilder.length() > 0) {
+            System.out.println("### 改进皮肤\n");
+            System.out.println(skinBuilder.toString());
+        }
+
+        if (themeBuilder.length() > 0) {
+            System.out.println("### 改进主题\n");
+            System.out.println(themeBuilder.toString());
+        }
+
+        if (enhancementBuilder.length() > 0) {
+            System.out.println("### 改进功能\n");
+            System.out.println(enhancementBuilder.toString());
+        }
+
+        if (docBuilder.length() > 0) {
+            System.out.println("### 文档相关\n");
+            System.out.println(docBuilder.toString());
+        }
+
+        if (developmentBuilder.length() > 0) {
+            System.out.println("### 开发重构\n");
+            System.out.println(developmentBuilder.toString());
+        }
+
+        if (bugBuilder.length() > 0) {
+            System.out.println("### 修复缺陷\n");
+            System.out.println(bugBuilder.toString());
+        }
+
+        System.out.println(count + " issues totally.");
+    }
+
+    private static JSONArray LABLES;
+
+    private static void loadLables() throws Exception {
+        System.out.println("Loading labels....");
+        final CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        final HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + REPOS + "/labels");
+        final HttpResponse response = httpClient.execute(httpGet);
+        final HttpEntity entity = response.getEntity();
+        final String content = IOUtils.toString(entity.getContent(), StandardCharsets.UTF_8);
+        LABLES = new JSONArray(content);
+    }
+
+    private static String getVersion() throws Exception {
+        final CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        final HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + REPOS + "/milestones/" + MILESTONE_NUM);
+        final HttpResponse response = httpClient.execute(httpGet);
+        final HttpEntity entity = response.getEntity();
+        final String content = IOUtils.toString(entity.getContent(), StandardCharsets.UTF_8);
+        final JSONObject milestone = new JSONObject(content);
+        return milestone.optString("title");
+    }
+
+    private static JSONObject getLable(final String labelName) {
+        for (int i = 0; i < LABLES.length(); i++) {
+            final JSONObject label = LABLES.optJSONObject(i);
+            if (label.optString("name").equals(labelName)) {
+                return label;
+            }
+        }
+        return null;
     }
 }
